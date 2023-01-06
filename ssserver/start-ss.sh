6aaa465e1d0d6d9c038b935dafece3f535aa6dc8
Your Name <you@example.com>
2023-01-06 07:13:58 +0000

    .

--- ssserver/start-ss.sh
+++ ssserver/start-ss.sh
@@ -2,16 +2,15 @@
 
 set -xe
 
-
+#exec kcptun_linux_amd64 -t "127.0.0.1:8443" -l ":4000" -mode fast2
 
 exec ssserver \
     -s "0.0.0.0:8443" \
     -k $PASSWORD \
     -m $METHOD \
     --timeout $TIMEOUT \
-    --dns $DNS_ADDRS
-
-exec server_linux_amd64 -t "127.0.0.1:8443" -l ":4000" -mode fast2
+    --dns $DNS_ADDRS &
+exec kcptun_linux_amd64  -t 127.0.0.1:8443 --listen 0.0.0.0:16384–16387 -mode fast3
 # exec ssserver \
 #     -s "0.0.0.0:8443" \
 #     -k $PASSWORD \
